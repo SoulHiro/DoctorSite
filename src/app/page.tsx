ff137d894703b73e2401a70643ba94c388727d9e
SoulHiro <victormts.s1@gmail.com>
2025-07-02 13:27:52 -0300

    chore(custom): default file cleanup and initial setup to start authentication feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center bg-red-500">
+    <div>
       <h1>Hello World!</h1>
     </div>
   )
